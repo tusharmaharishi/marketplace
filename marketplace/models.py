91676fd65c867608deb428323018532fce720061
Tushar Maharishi <tm5gf@virginia.edu>
2017-02-08 22:47:51 -0500

    updated models.py with user and carpool models

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.TextField()
+    user_id = models.TextField()
+
+class Carpool(models.Model):
+    carpool_id = models.TextField()
+    driver = models.TextField() #single driver (user)
+    passengers = models.TextField() #list of passengers (users)
+    cost = models.TextField()
+    location_start = models.TextField()
+    location_end = models.TextField()
+
