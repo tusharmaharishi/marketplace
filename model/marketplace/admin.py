dae1c0ead05dbbfbad5b4ba85d25fd8001a0b815
bryanchen18 <bryanchen@email.virginia.edu>
2017-02-23 18:55:58 -0500

    admins

--- model/marketplace/admin.py
+++ model/marketplace/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Carpool
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+class CarpoolAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Carpool, CarpoolAdmin)
