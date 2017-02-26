0f2c41c372e186983dff87e3b53676cc23016660
bryanchen18 <bryanchen@email.virginia.edu>
2017-02-26 01:05:12 -0500

    list users template

--- web/client/views.py
+++ web/client/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 import urllib.request
 import urllib.parse
-import requests
 import json
+import requests
 
 BASE_API = 'http://model-api:8000/v1/'
 
 def list_users(request):
-    res = requests.get(BASE_API + 'users/3/')
-    return render(request, 'list_user.html', {'user_list': res})
+    # res = requests.get(BASE_API + 'users/')
+    # return render(request, 'list_user.html', {'user_list': res})
+    req = urllib.request.Request(BASE_API + 'users/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    res = json.loads(resp_json)
+    return render(request, 'list_user.html', {'user_list': res}, content_type='application/json')
