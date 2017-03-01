941854c704468d881d448a31a103ec6c00fac00c
bryanchen18 <bryanchen@email.virginia.edu>
2017-02-28 20:38:55 -0500

    base gmaps template

--- web/client/views.py
+++ web/client/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-import urllib.request
-import urllib.parse
-import json
 import requests
+from django.shortcuts import render
 
 BASE_API = 'http://model-api:8000/v1/'
+
+
+def index(request):
+    return render(request, 'index.html')
+
 
 def list_users(request):
     res = requests.get(BASE_API + 'users/2/').json()
