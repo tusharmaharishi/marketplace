9401729a27116dfd2662910179ea18fb1fd740bf
bryanchen18 <bryanchen@email.virginia.edu>
2017-02-28 22:27:11 -0500

    static files working

--- model/marketplace/tests.py
+++ model/marketplace/tests.py
@@ -14,20 +14,20 @@ class GetUserTestCase(TestCase):
         # checks that response contains parameter order list & implicitly
         # checks that the HTTP status code is 200
         jsonResponse = json.loads(str(response.content, encoding='utf8'))
-        self.assertEquals(jsonResponse["status"], '200 OK')
+        self.assertEquals(jsonResponse['status'], '200 OK')
         self.assertContains(response, 'pk')
         self.assertEqual(response.status_code, 200)
 
     def test_fails_invalid(self):
         response = self.client.get(reverse('user_detail', kwargs={'pk': 0}))
         jsonResponse = json.loads(str(response.content, encoding='utf8'))
-        self.assertEquals(jsonResponse["status"], '404 Not Found')
+        self.assertEquals(jsonResponse['status'], '404 Not Found')
         self.assertEquals(response.status_code, 404)
 
     def test_user_fields(self):
         response = self.client.get(reverse('user_detail', kwargs={'pk': 1}))
         jsonResponse = json.loads(str(response.content, encoding='utf8'))
-        self.assertEquals(jsonResponse["status"], '200 OK')
+        self.assertEquals(jsonResponse['status'], '200 OK')
         self.assertContains(response, 'pk')
         self.assertContains(response, 'name')
         self.assertContains(response, 'balance')
