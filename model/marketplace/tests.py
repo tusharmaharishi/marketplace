0f1bfbbfd08ede3d9797d835a7c17f0e1bf7f1f1
bryanchen18 <bryanchen@email.virginia.edu>
2017-03-01 00:11:34 -0500

    test users

--- model/marketplace/tests.py
+++ model/marketplace/tests.py
@@ -1,38 +1,52 @@
 import json
 
-from django.core.urlresolvers import reverse
-from django.test import TestCase
+from django.test import TestCase, Client
+
+BASE_API = 'http://model-api:8000/'
 
 
 class GetUserTestCase(TestCase):
     def setUp(self):
-        pass  # nothing to set up
+        self.client = Client()
 
     def test_success_response(self):
         # assumes user with id 1 is stored in db
-        response = self.client.get(reverse('user_list'))
+        response = self.client.get('/v1/users/')
         # checks that response contains parameter order list & implicitly
         # checks that the HTTP status code is 200
-        jsonResponse = json.loads(str(response.content, encoding='utf8'))
-        self.assertEquals(jsonResponse['status'], '200 OK')
-        self.assertContains(response, 'pk')
+        response_json = json.loads(str(response.content, encoding='utf8'))
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'count')
+        self.assertContains(response, 'data')
 
-    def test_fails_invalid(self):
-        response = self.client.get(reverse('user_detail', kwargs={'pk': 0}))
-        jsonResponse = json.loads(str(response.content, encoding='utf8'))
-        self.assertEquals(jsonResponse['status'], '404 Not Found')
-        self.assertEquals(response.status_code, 404)
+    def test_fail_response(self):
+        response = self.client.get('/v1/users/0/')
+        response_json = json.loads(str(response.content, encoding='utf8'))
+        self.assertEqual(response_json['status'], '404 Not Found')
+        self.assertEqual(response.status_code, 404)
 
-    def test_user_fields(self):
-        response = self.client.get(reverse('user_detail', kwargs={'pk': 1}))
-        jsonResponse = json.loads(str(response.content, encoding='utf8'))
-        self.assertEquals(jsonResponse['status'], '200 OK')
-        self.assertContains(response, 'pk')
-        self.assertContains(response, 'name')
-        self.assertContains(response, 'balance')
-
+    def test_user_get(self):
+        response = self.client.get('/v1/users/1/')
+        response_json = json.loads(str(response.content, encoding='utf8'))
+        data = response_json['data'][0]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['pk'], 1)
+        # self.assertContains(response_json['data']['fields']['name'], 'Kevin')
+        # self.assertContains(response_json['data'], 'balance')
         # tearDown method is called after each test
+
+    # def test_user_post(self):
+    #     response = self.client.post('/v1/users/', {'name': 'Tester', 'balance': 10.00})
+    #
+
 
     def tearDown(self):
         pass  # nothing to tear down
+
+
+class GetCarpoolTestCAse(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def tearDown(self):
+        pass
