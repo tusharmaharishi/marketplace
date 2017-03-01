267c07fc71e19c0ed825e66822f08299c4ffd7e2
bryanchen18 <bryanchen@email.virginia.edu>
2017-02-28 20:13:49 -0500

    changed to model api

--- web/client/views.py
+++ web/client/views.py
@@ -4,8 +4,9 @@ import urllib.parse
 import json
 import requests
 
-BASE_API = 'http://exp-api:8000/v1/'
+BASE_API = 'http://model-api:8000/v1/'
 
 def list_users(request):
-    res = requests.get(BASE_API + 'users/')
-    return render(request, 'list_user.html', {'user_list': res.json()})
+    res = requests.get(BASE_API + 'users/2/').json()
+    data = res['data']
+    return render(request, 'list_user.html', {'user_list': data})
