a1e48c4638a8580b321e80c95a56c935f384d1a6
bryanchen18 <bryanchen@email.virginia.edu>
2017-04-07 09:38:50 -0400

    fixed makedb cleardb scripts, docker-compose set up mysql and mysql-cmdline successfully with pip install

--- script/makedb.sh
+++ script/makedb.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-sleep 40
+sleep 20
 
 if [[ -e /app/db/cs4501 ]] ; then
     mysql -uroot -p'$3cureUS' -h db -Bse \
-    "drop database cs4501;
+    "drop database if exists cs4501;
     create database cs4501 character set utf8;
     grant all on *.* to 'www'@'%'; \
     ";
 else
     mysql -uroot -p'$3cureUS' -h db -Bse \
-    "create user 'www'@'%' identified by '\$3cureUS';
+    "drop user if exists 'www'@'%';
+    flush privileges;
+    drop database if exists cs4501;
+    create user 'www'@'%' identified by '\$3cureUS';
     create database cs4501 character set utf8;
     grant all on *.* to 'www'@'%'; \
     ";
 fi
 
-echo "Success!"
+echo "Successfully created 'cs4501' table with root user 'www'@'%'"
--- script/resetdb.sh
+++ script/resetdb.sh
@@ -2,17 +2,17 @@
 set -e
 
 docker rm -f exp model es kafka batch web &> /dev/null || {
-    echo "Some container(s) cannot be removed."
+    echo "Failed to remove some container(s)"
     echo "Try docker ps -a"
 }
 
 docker stop mysql mysql-cmdline &> /dev/null {
-    echo "There was an error stopping mysql container(s)."
+    echo "Failed to stop mysql container(s)"
     echo "Try docker ps -a"
 }
 
 docker start mysql mysql-cmdline &> /dev/null || {
-    echo "There was an error starting mysql container(s)."
+    echo "Failed to start mysql container(s)."
     exit 1
 }
 
@@ -22,13 +22,13 @@ docker exec -it mysql-cmdline bash -c \
 "mysql -uroot -p'\$3cureUS' -h db -Bse \"drop database cs4501;
 create database cs4501 character set utf8;
 grant all on *.* to 'www'@'%'; \"; " &> /dev/null || {
-    echo "There was an error clearing mysql database."
+    echo "Failed to clear mysql database."
     exit 2
 }
 
-echo "Database was reset successfully."
+echo "Successfully reset mysql database"
 
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 
-echo "Migrations were deleted successfully."
+echo "Successfully deleted migrations"
