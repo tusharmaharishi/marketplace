ada4faa970f507590f6ce088936381704ddadeb6
Bryan Chen <bryanchen@email.virginia.edu>
2017-04-26 17:18:54 -0400

    need chromedriver 2.29 in droplet, but chromedriver 2.19 in root computer, bad status line error because laptop out of memory

--- web/client/tests.py
+++ web/client/tests.py
@@ -1,51 +1,55 @@
+import os
 import unittest
 from unittest import TestCase
-from pyvirtualdisplay import Display
+
+# from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import os
+
 cwd = os.getcwd()
 print(cwd)
 
+
 class AuthenticationTestCase(TestCase):
     def setUp(self):
-	display = Display(visible=0, size=(800, 800))  
-	display.start()
-        driver = webdriver.Chrome('/root/marketplace/web/client/chromedriver')
+        # display = Display(visible=0, size=(800, 800))
+        # display.start()
+        self.driver = webdriver.Chrome('./chromedriver')
 
     def test_login(self):
-        driver = self.driver
-        driver.get("http://107.170.77.19:8000/login")
-	print('getting test log')
-        username = driver.find_element_by_id("username")
+        self.driver = self.driver
+        self.driver.get("http://107.170.77.19:8000/login")
+        print('getting test log')
+        username = self.driver.find_element_by_id("username")
         username.send_keys("abc")
 
-        password = driver.find_element_by_id("password")
+        password = self.driver.find_element_by_id("password")
         password.send_keys("abc")
 
-        driver.find_element_by_xpath("//button[@type='submit' and @value='Submit']").click()
+        self.driver.find_element_by_xpath("//button[@type='submit' and @value='Submit']").click()
 
     def test_logout(self):
-        driver = self.driver
-        driver.get("http://107.170.77.19:8000/login")
-	print('test logout')
-        username = driver.find_element_by_id("username")
+        self.driver = self.driver
+        self.driver.get("http://107.170.77.19:8000/login")
+        print('test logout')
+        username = self.driver.find_element_by_id("username")
         username.send_keys("abc")
 
-        password = driver.find_element_by_id("password")
+        password = self.driver.find_element_by_id("password")
         password.send_keys("abc")
 
-        driver.find_element_by_xpath("//button[@type='submit' and @value='Submit']").click()
+        self.driver.find_element_by_xpath("//button[@type='submit' and @value='Submit']").click()
 
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "container-fluid")))
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "container-fluid")))
 
-        driver.find_element_by_xpath("//a[@href='/logout']").click()
+        self.driver.find_element_by_xpath("//a[@href='/logout']").click()
 
     def tearDown(self):
-      	driver.close()
-	display.stop()
+        self.driver.close()
+        # display.stop()
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
